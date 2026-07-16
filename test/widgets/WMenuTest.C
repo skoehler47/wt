45f2e43ffd24d896c348539bbf2ac53372d802b7
Romain Mardulyn <romain@legion.leuven.emweb.be>
2026-07-16 14:34:17 +0200

    WT-14677: Add test for WMenu selection propagation

--- test/widgets/WMenuTest.C
+++ test/widgets/WMenuTest.C
@@ -11,6 +11,69 @@
 #include <Wt/WStackedWidget.h>
 #include <Wt/WText.h>
 
+BOOST_AUTO_TEST_CASE(WMenu_select_propagation_to_parent_and_submenus)
+{
+  Wt::Test::WTestEnvironment testEnv;
+  Wt::WApplication app(testEnv);
+
+  Wt::WStackedWidget *stack = app.root()->addNew<Wt::WStackedWidget>();
+  Wt::WMenu *menu = app.root()->addNew<Wt::WMenu>(stack);
+
+  Wt::WMenuItem *item0 = menu->addItem("Item 0", std::make_unique<Wt::WText>("Content 0"));
+
+  auto subMenuPtr_ = std::make_unique<Wt::WMenu>(stack);
+  Wt::WMenu* subMenu = subMenuPtr_.get();
+  subMenu->addItem("Item 1.1", std::make_unique<Wt::WText>("Content 1.1"));
+
+  auto subSubMenuPtr_ = std::make_unique<Wt::WMenu>(stack);
+  Wt::WMenu* subSubMenu = subSubMenuPtr_.get();
+  subSubMenu->addItem("Item 1.2.1", std::make_unique<Wt::WText>("Content 1.2.1"));
+  subSubMenu->addItem("Item 1.2.2", std::make_unique<Wt::WText>("Content 1.2.2"));
+  Wt::WMenuItem *item1_2_3 = subSubMenu->addItem("Item 1.2.3", std::make_unique<Wt::WText>("Content 1.2.3"));
+
+  Wt::WMenuItem *item1_2 = subMenu->addMenu("Item 1.2", std::move(subSubMenuPtr_));
+  subMenu->addItem("Item 1.3", std::make_unique<Wt::WText>("Content 1.3"));
+
+  Wt::WMenuItem *item1 = menu->addMenu("Item 1", std::move(subMenuPtr_));
+  menu->addItem("Item 2", std::make_unique<Wt::WText>("Content 2"));
+
+  // By default, the first item of the top menu is selected.
+  BOOST_TEST(menu->currentItem() == item0);
+  BOOST_TEST(subMenu->currentItem() == nullptr);
+  BOOST_TEST(subSubMenu->currentItem() == nullptr);
+  BOOST_REQUIRE_EQUAL(menu->currentIndex(), 0);
+  BOOST_REQUIRE_EQUAL(subMenu->currentIndex(), -1);
+  BOOST_REQUIRE_EQUAL(subSubMenu->currentIndex(), -1);
+
+  // Select should be propagated to the parent menus.
+  subSubMenu->select(2);
+  BOOST_TEST(menu->currentItem() == item1);
+  BOOST_TEST(subMenu->currentItem() == item1_2);
+  BOOST_TEST(subSubMenu->currentItem() == item1_2_3);
+  BOOST_REQUIRE_EQUAL(menu->currentIndex(), 1);
+  BOOST_REQUIRE_EQUAL(subMenu->currentIndex(), 1);
+  BOOST_REQUIRE_EQUAL(subSubMenu->currentIndex(), 2);
+
+  // Select should not be propagated to the submenus.
+  menu->select(0);
+  BOOST_TEST(menu->currentItem() == item0);
+  BOOST_TEST(subMenu->currentItem() == item1_2);
+  BOOST_TEST(subSubMenu->currentItem() == item1_2_3);
+  BOOST_REQUIRE_EQUAL(menu->currentIndex(), 0);
+  BOOST_REQUIRE_EQUAL(subMenu->currentIndex(), 1);
+  BOOST_REQUIRE_EQUAL(subSubMenu->currentIndex(), 2);
+
+  // Select should be propagated to the parent menu even when the index did not change.
+  subSubMenu->select(2);
+  BOOST_TEST(menu->currentItem() == item1);
+  BOOST_TEST(subMenu->currentItem() == item1_2);
+  BOOST_TEST(subSubMenu->currentItem() == item1_2_3);
+  BOOST_TEST(menu->currentIndex() == 1);
+  BOOST_TEST(subMenu->currentIndex() == 1);
+  BOOST_TEST(subSubMenu->currentIndex() == 2);
+
+}
+
 BOOST_AUTO_TEST_CASE(WMenu_addItem_change_index_for_internal_path_match)
 {
   Wt::Test::WTestEnvironment testEnv;
